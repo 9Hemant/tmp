bfb7cadd1edb9f0f72016ab07c7f4d4643dd8e2b
9Hemant <96587606+9Hemant@users.noreply.github.com>
2023-05-23 23:29:06 +0530

    Add files via upload

---
+++ Assignment2-A.c
@@ -0,0 +1,93 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include<sys/wait.h>
+int a[10],n,i,j,temp;
+void getarray()
+{
+printf("Enter size of array :");
+scanf("%d",&n);
+printf("Enter array elements :");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+}
+void bsort()
+{
+for (i=0;i<n-1;i++)
+{
+for(j=0;j<n-i-1;j++)
+{
+if(a[j+1]<a[j])
+{
+temp=a[j];
+a[j]=a[j+1];
+a[j+1]=temp;
+}
+}
+}
+}
+void selectsort()
+{
+int smallest;
+for (i=0;i<n-1;i++)
+{
+smallest=i;
+for(j=i+1;j<n;j++)
+{
+if(a[smallest]>a[j])
+{
+smallest=j;
+}
+}
+temp=a[smallest];
+a[smallest]=a[i];
+a[i]=temp;
+}
+}
+void insertion()
+{
+for (i=1;i<n;i++)
+{
+temp=a[i];
+j=i-1;
+while(j>=0 && temp<=a[j])
+{
+a[j+1]=a[j];
+j=j-1;
+}
+a[j+1]=temp;
+}
+}
+void display()
+{
+for (i=0;i<n;i++)
+printf("%d ",a[i]);
+}
+int main()
+{
+getarray();
+pid_t pid;
+printf("\nForking .....\n");
+pid=fork();
+if(pid==0)
+{
+printf("\nChild Process in Execution \n");
+bsort();
+display();
+printf("\nChild terminated\n");
+}
+else if(pid<0)
+printf("ERROR!!");
+else
+{
+//wait(NULL);
+printf("\nParent Process in Execution \n");
+selectsort();
+display();
+printf("\nParent terminated\n");
+}
+}
---
+++ Assignment2-Bchild.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+int main(int argc,char *argv[])
+{
+printf("Descending Array:");
+for(int j=argc-1;j>=0;j--)
+{
+printf("%s ",argv[j]);
+}printf("\n");
+exit(EXIT_SUCCESS);
+}
